f1c16bad23846fe418e71332967f8d1fcd6b26d8
Raiwin Inthasit <andaraiwin@gmail.com>
2021-11-26 17:54:44 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <cmath> 
 
 using namespace std;
+
+double pi = M_PI;
+
+double deg2rad(double x){
+    
+    double y = x * (pi/180.0);
+    return y;
+}
+
+double rad2deg(double Z){
+    double w = Z * (180.0/pi);
+    return w;
+}
+
+double findXComponent(double l1,double l2,double A1,double A2){
+    double result1 = l1 *cos(A1) + l2 *cos(A2);
+    return result1;
+}
+
+double findYComponent(double l1,double l2,double A1,double A2){
+    double result2 = l1 *sin(A1) + l2 *sin(A2);
+    return result2;
+}
+
+double pythagoras(double Line1 ,double Line2){
+    double u = sqrt(pow(Line1, 2) + pow(Line2, 2));
+    return u;
+}
+
+void showResult(double result_Length,double result_direction){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+    cout << "\nLength of the resultant vector = " << result_Length;
+    cout << "\nDirection of the resultant vector (deg) = " << result_direction;
+    cout << "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+} 
+
+
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
