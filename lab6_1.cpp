bcc19f7b868d121cd11774d1521ffc9df369ce9f
Raiwin Inthasit <andaraiwin@gmail.com>
2021-11-26 17:39:19 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    int N = 1 , E = 0 , O = 0;
+
+    while(N != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+        if(N%2 == 0 && N != 0){
+            E++;
+        }
+        if(N%2 != 0){
+            O++;
+        }
+    }
+    cout << "#Even numbers = " << E;
+    cout << "\n#Odd numbers = " << O;
+
     return 0;
 }
